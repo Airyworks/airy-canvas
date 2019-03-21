d731c3773d6d1df09c4906b7772279a4ca4cc11f
tari404 <2680383605@qq.com>
2019-03-21 21:39:17 +0800

     🖍 update Pen brush

--- src/model/brush/Pen.js
+++ src/model/brush/Pen.js
@@ -47,8 +47,8 @@ const computeOffset = (pl, pr, p) => {
   const offset = area / base
   return offset
 }
-const offset = (p1, p2, p3, p4) => {
-  const mergerP = {
+const offset = (p1, p2, p3, p4, mergerP) => {
+  mergerP = mergerP || {
     x: (p2.x + p3.x) / 2,
     y: (p2.y + p3.y) / 2
   }
@@ -56,28 +56,41 @@ const offset = (p1, p2, p3, p4) => {
   const d2 = computeOffset(mergerP, p4, p3)
   return Math.abs(d1 + d2)
 }
+// calculate a coefficient to indicate the distortion
+// at the sharp corner after merging points
+const computeSharpLoss = (p1, p2, p3, p4, mergerP) => {
+  mergerP = mergerP || {
+    x: (p2.x + p3.x) / 2,
+    y: (p2.y + p3.y) / 2
+  }
+  const d1 = Math.abs(distance(p1, p2) - distance(p1, mergerP))
+  const d2 = Math.abs(distance(p3, p4) - distance(p4, mergerP))
+  return Math.abs(d1 - d2)
+}
 
 const simplify2 = points => {
   const s = [...points]
-  const threshold = 0.4
+  const threshold = 0.6
   let i = 1
   let iteration = true
-  let iterationCount = 0
-  while (iteration && iterationCount < 100) {
+  while (iteration) {
     i = 1
     iteration = false
-    iterationCount++
     while (i < s.length - 3) {
       const leftP = s[i - 1]
       const rightP = s[i + 2]
-      const d = offset(leftP, s[i], s[i + 1], rightP)
+      const mergerP = {
+        x: (s[i].x + s[i + 1].x) / 2,
+        y: (s[i].y + s[i + 1].y) / 2
+      }
+      const d = offset(leftP, s[i], s[i + 1], rightP, mergerP)
       if (d < threshold) {
-        iteration = true
-        s[i] = {
-          x: (s[i].x + s[i + 1].x) / 2,
-          y: (s[i].y + s[i + 1].y) / 2
+        const sharpLoss = computeSharpLoss(leftP, s[i], s[i + 1], rightP, mergerP)
+        if (sharpLoss < threshold / 2) {
+          iteration = true
+          s[i] = mergerP
+          s.splice(i + 1, 1)
         }
-        s.splice(i + 1, 1)
       }
       i++
     }
@@ -112,7 +125,7 @@ const genControlPoints = points => {
       return 0
     }
     const prevP = points[prev]
-    return Math.sqrt(Math.pow(p.x - prevP.x, 2) + Math.pow(p.y - prevP.y, 2))
+    return distance(p, prevP)
   })
   let p1 = points[0]
   const controlPoints = points.map((p, i, points) => {
@@ -127,13 +140,14 @@ const genControlPoints = points => {
     const v1 = [(p1.x - p.x) / dis[i], (p1.y - p.y) / dis[i]]
     const v2 = [(p2.x - p.x) / dis[next], (p2.y - p.y) / dis[next]]
     const tangent = [v2[0] - v1[0], v2[1] - v1[1]]
+    const min = Math.min(dis[i], dis[next])
     const left = {
-      x: p.x - tangent[0] * dis[i] * 0.15,
-      y: p.y - tangent[1] * dis[i] * 0.15
+      x: p.x - tangent[0] * min * 0.16,
+      y: p.y - tangent[1] * min * 0.16
     }
     const right = {
-      x: p.x + tangent[0] * dis[next] * 0.15,
-      y: p.y + tangent[1] * dis[next] * 0.15
+      x: p.x + tangent[0] * min * 0.16,
+      y: p.y + tangent[1] * min * 0.16
     }
     p1 = p
     return { left, right }
@@ -257,19 +271,19 @@ export default class Pen {
     ctx.lineCap = 'round'
     ctx.lineJoin = 'round'
     ctx.stroke()
-    ctx.save()
-    ctx.globalAlpha = 1
-    ctx.fillStyle = '#000'
-    this.path.forEach(point => {
-      ctx.fillRect(point.x - 2, point.y - 2, 4, 4)
-    })
-    ctx.globalAlpha = 0.3
-    ctx.fillStyle = '#f00'
-    this.ctrlPoints.forEach(point => {
-      ctx.fillRect(point.right.x - 2, point.right.y - 2, 4, 4)
-      ctx.fillRect(point.left.x - 2, point.left.y - 2, 4, 4)
-    })
-    ctx.restore()
+    // ctx.save()
+    // ctx.globalAlpha = 1
+    // ctx.fillStyle = '#000'
+    // this.path.forEach(point => {
+    //   ctx.fillRect(point.x - 2, point.y - 2, 4, 4)
+    // })
+    // ctx.globalAlpha = 0.3
+    // ctx.fillStyle = '#f00'
+    // this.ctrlPoints.forEach(point => {
+    //   ctx.fillRect(point.right.x - 2, point.right.y - 2, 4, 4)
+    //   ctx.fillRect(point.left.x - 2, point.left.y - 2, 4, 4)
+    // })
+    // ctx.restore()
     layer.layerImgData = ctx.getImageData(x, y, w, h)
     layer.x = x
     layer.y = y
